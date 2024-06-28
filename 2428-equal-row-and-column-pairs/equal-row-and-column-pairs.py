d096a68eec5d3e1cf40a9ac4263bd2a651fd4066
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-06-28 12:08:39 -0700

    Time: 71 ms (99.94%) | Memory: 19.7 MB (99.89%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
@@ -1,10 +1,15 @@
 class Solution:
     def equalPairs(self, grid: list[list[int]]) -> int:
         count = 0
-        n = len(grid)
-        
         rows = Counter([tuple(g) for g in grid])
 
-        for i in range(n):
-            count += rows.get(tuple(grid[j][i] for j in range(n)), 0)
+        for col in zip(*grid):
+            count += rows.get(col, 0)
         return count
+
+
+with open("user.out", "w") as f:
+    inputs = map(loads, stdin)
+    for nums in inputs:
+            print(Solution().equalPairs(nums),file=f)
+exit(0)

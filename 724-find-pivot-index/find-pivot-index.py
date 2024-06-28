a5ee7fcbe806c8f8a8ab67e0982bfe8baad50c8c
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-06-27 20:30:52 -0700

    Time: 118 ms (78.26%) | Memory: 17.6 MB (82.44%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -1,13 +1,11 @@
 class Solution:
     def pivotIndex(self, nums: list[int]) -> int:
-        n = len(nums)
-        prefix = [0]
-        suffix = [0]
-        for i in range(0, n-1):
-            prefix.append(nums[i] + prefix[-1])
-        for i in range(n-1, 0, -1):
-            suffix.insert(0, nums[i] + suffix[0])
-        for i in range(n):
-            if prefix[i] == suffix[i]:
+        lsum = 0
+        rsum = sum(nums)
+
+        for i, e in enumerate(nums):
+            rsum -= e
+            if rsum == lsum:
                 return i
+            lsum += e
         return -1

1c0bd5807179c726fbf97ebd85fc28dbab09b8a2
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-07-01 18:21:31 -0700

    Time: 52 ms (75.45%) | Memory: 16.9 MB (72.18%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -3,7 +3,7 @@ class Solution:
         parent = [i for i in range(len(edges) + 1)]
         rank = [1] * (len(edges) + 1)
 
-        def find(node):
+        def find_parent(node):
             p = parent[node]
 
             while p != parent[p]:
@@ -12,18 +12,17 @@ class Solution:
             return p
         
         def union(n1, n2):
-            p1, p2 = find(n1), find(n2)
+            p1, p2 = find_parent(n1), find_parent(n2)
             if p1 == p2:
                 return False
-            
             if rank[p1] >= rank[p2]:
-                parent[p2] = p1
                 rank[p1] += rank[p2]
+                parent[p2] = p1
             else:
-                parent[p1] = p2
                 rank[p2] += rank[p1]
+                parent[p1] = p2
             return True
-
+        
         for n1, n2 in edges:
             if not union(n1, n2):
                 return [n1, n2]

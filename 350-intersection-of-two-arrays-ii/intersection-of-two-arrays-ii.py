cbbd9e5c3042ba4f68157c6201e616925a5590e4
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-07-01 19:07:58 -0700

    Time: 42 ms (88.36%) | Memory: 16.7 MB (71.02%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -4,7 +4,8 @@ class Solution:
         nums2.sort()
         p1 = p2 = 0
         ans = []
-        while p1 < len(nums1) and p2 < len(nums2):
+        ln1, ln2 = len(nums1), len(nums2)
+        while p1 < ln1 and p2 < ln2:
             if nums1[p1] < nums2[p2]: p1 += 1
             elif nums1[p1] > nums2[p2]: p2 += 1
             else: ans.append(nums1[p1]); p1 += 1; p2 += 1

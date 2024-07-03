bcd5766a84ca1e226b7fa9b7b4492d3e8c2333b0
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-07-03 12:51:34 -0700

    Time: 569 ms (5.02%) | Memory: 50.1 MB (6.67%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
@@ -2,15 +2,15 @@ import numpy as np
 
 class Solution:
     def maxScore(self, cardPoints: list[int], k: int) -> int:
-        prefixsum = list(np.cumsum(cardPoints))
-        suffixsum = list(np.cumsum(cardPoints[::-1]))
+        prefixsum = deque(np.cumsum(cardPoints))
+        suffixsum = deque(np.cumsum(cardPoints[::-1]))
         ans = ps = ss = 0
         while k > 0:
             if  prefixsum[k-1] - ps > suffixsum[k-1] - ss:
-                card = prefixsum.pop(0) - ps
+                card = prefixsum.popleft() - ps
                 ps += card
             else:
-                card = suffixsum.pop(0) - ss
+                card = suffixsum.popleft() - ss
                 ss += card
             ans += card
             k -= 1

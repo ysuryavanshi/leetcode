cceaac1783c22cd3282a6b090247f31b990d99fe
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-07-08 16:19:14 -0700

    Time: 44 ms (7.13%) | Memory: 16.5 MB (77.86%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.py
+++ 2645-pass-the-pillow/pass-the-pillow.py
@@ -1,6 +1,7 @@
 class Solution:
     def passThePillow(self, n: int, time: int) -> int:
         d = i = 1
+        time = time % (2 * (n - 1))
         while time:
             time -= 1
             i += d

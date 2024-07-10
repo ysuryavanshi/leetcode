cd431c620f85ac69306f0ebff82f678d574e9183
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-07-10 16:52:42 -0700

    Time: 52 ms (53.07%) | Memory: 17.1 MB (27.09%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def pathSum(self, root, targetSum) -> int:
+        sums = defaultdict(int)
+        sums[0] = 1
+
+        def dfs(root, total):
+            count = 0
+            if root:
+                total += root.val
+                count = sums[total - targetSum]
+                
+                sums[total] += 1
+                count += dfs(root.left, total) + dfs(root.right, total)
+                sums[total] -= 1
+            return count
+
+        return dfs(root, 0)

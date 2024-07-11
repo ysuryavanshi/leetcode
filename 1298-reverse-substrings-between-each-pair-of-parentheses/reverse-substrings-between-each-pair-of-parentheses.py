e7abe13a4bd565f5a9d96ed35e9a1db7a5312bbc
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-07-10 17:48:01 -0700

    Time: 36 ms (54.02%) | Memory: 16.6 MB (40.89%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.py
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.py
@@ -1,29 +1,14 @@
 class Solution:
     def reverseParentheses(self, s: str) -> str:
-        n = len(s)
-        open_parentheses_indices = []
-        pair = [0] * n
-
-        # First pass: Pair up parentheses
-        for i in range(n):
-            if s[i] == "(":
-                open_parentheses_indices.append(i)
-            if s[i] == ")":
-                j = open_parentheses_indices.pop()
-                pair[i] = j
-                pair[j] = i
-
-        # Second pass: Build the result string
-        result = []
-        curr_index = 0
-        direction = 1
-
-        while curr_index < n:
-            if s[curr_index] == "(" or s[curr_index] == ")":
-                curr_index = pair[curr_index]
-                direction = -direction
+        stack = []
+        for c in s:
+            if c == ')':
+                ss = []
+                while stack[-1] != '(':
+                    ss.append(stack.pop(-1))
+                else:
+                    _ = stack.pop(-1)
+                stack.extend(ss)
             else:
-                result.append(s[curr_index])
-            curr_index += direction
-
-        return "".join(result)
+                stack.append(c)
+        return ''.join(stack)

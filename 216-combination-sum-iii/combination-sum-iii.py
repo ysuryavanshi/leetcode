88e3b6d21b8c239f1b67d3df4d92dd719e947c11
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-09-05 07:09:43 -0700

    Time: 34 ms (71.34%) | Memory: 16.5 MB (74.89%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.py
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -1,17 +1,5 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        ans = []
-
-        def backtrack(num, stack, target):
-            if len(stack) == k:
-                if target == 0:
-                    ans.append(stack)
-                return
-            
-            for i in range(num + 1, 10):
-                if i <= target:
-                    backtrack(i, stack + [i], target - i)
-                else: return
-
-        backtrack(0, [], n)
+        all_c = list(combinations(range(1,10), k))
+        ans = [c for c in all_c if sum(c) == n]
         return ans

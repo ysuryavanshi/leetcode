6273ab804761490e2cd83fcf1773f0ee9573c06b
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-09-05 06:23:24 -0700

    Time: 980 ms (66.31%) | Memory: 27.5 MB (27.01%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.py
+++ 2155-find-missing-observations/find-missing-observations.py
@@ -2,20 +2,6 @@ class Solution:
     def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
         r = ((n + len(rolls)) * mean) - sum(rolls)
 
-        base_roll, r = divmod(r, n)
-        if base_roll <= 0 or base_roll > 6:
-            return []
-        
-        ans = [base_roll] * n
-        for i, a in enumerate(ans):
-            if a < 6:
-                diff = 6 - a
-                if r >= diff:
-                    ans[i] = 6
-                    r -= diff
-                else:
-                    ans[i] += r
-                    r = 0
-        
-        if r > 0: return []
-        return ans
+        if r > 6 * n or r < n: return []
+        q, r = divmod(r, n)
+        return [q + 1] * r + [q] * (n-r)

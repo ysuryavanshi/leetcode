b6d8f6b606a7b9cb58246350772b77e023a5a05b
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-09-10 21:30:13 -0700

    Time: 37 ms (45.20%) | Memory: 16.4 MB (69.15%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
@@ -1,13 +1,7 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
         flips = 0
-        while start > 0 or goal > 0:
-            s = start & 1
-            g = goal & 1
-
-            if s != g:
-                flips += 1
-            
-            start >>= 1
-            goal >>= 1
+        while start or goal:
+            if start % 2 != goal % 2: flips += 1
+            start, goal = start // 2, goal //2
         return flips

632df6883b4938098b112494626c26f7eb196bf4
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-09-12 06:56:32 -0700

    Time: 880 ms (69.67%) | Memory: 30.8 MB (85.68%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,14 +1,12 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        t_map = defaultdict(list)
-        for i, t in enumerate(temperatures):
-            t_map[t].append(i)
+        ans = [0] * len(temperatures)
+        stack = []
 
-        ans = [float('inf')] * (i + 1)
-        for i, t in enumerate(temperatures):
-            for j in range(t + 1, 101):
-                k = bisect.bisect_left(t_map[j], i)
-                if k < len(t_map[j]):
-                    ans[i] = min(ans[i], t_map[j][k] - i)
-            ans[i] = 0 if ans[i] == float('inf') else ans[i]
+        for i, temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temp:
+                index = stack.pop()
+                ans[index] = i - index
+            stack.append(i)
+
         return ans

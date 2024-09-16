ce5c834d7bc2eb430103c942b0475885e92a3605
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-09-16 09:59:52 -0700

    Time: 235 ms (88.22%) | Memory: 22.1 MB (33.42%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.py
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -1,27 +1,11 @@
 class Solution:
-    def findTheLongestSubstring(self, s: str) -> int:
-        mapp = [-2] * 32
-        mapp[0] = -1
-
-        max_len = 0
-        mask = 0
-
-        for i, char in enumerate(s):
-            if char == 'a':
-                mask ^= 1
-            elif char == 'e':
-                mask ^= 2
-            elif char == 'i':
-                mask ^= 4
-            elif char == 'o':
-                mask ^= 8
-            elif char == 'u':
-                mask ^= 16
-        
-            prev = mapp[mask]
-            if prev == -2:
-                mapp[mask] = i
-            else:
-                max_len = max(max_len, i - prev)
-        
-        return max_len
+    def findTheLongestSubstring(self, s):
+        mask_dict, mask, ans = {0: -1}, 0, 0
+        for i, c in enumerate(s):
+            if c == 'a': mask ^= 1
+            elif c == 'e': mask ^= 1 << 1
+            elif c == 'i': mask ^= 1 << 2
+            elif c == 'o': mask ^= 1 << 3
+            elif c == 'u': mask ^= 1 << 4
+            if (diff := i-mask_dict.setdefault(mask, i)) > ans:ans = diff
+        return ans

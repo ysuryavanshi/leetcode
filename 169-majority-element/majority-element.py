dd17309e99ccc8a78dd65769664d24caecdc0bb6
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-09-17 09:50:50 -0700

    Time: 173 ms (45.20%) | Memory: 18 MB (20.97%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,3 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return Counter(nums).most_common()[0][0]
+        d = defaultdict(int)
+
+        for num in nums:
+            d[num] += 1
+        
+        n = len(nums)//2
+        for key, value in d.items():
+            if value > n:
+                return key
+        return

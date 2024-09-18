4801c98c9d4f16e3ca5c7ed86c1fe053fe921c2c
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-09-18 07:20:51 -0700

    Time: 31 ms (96.03%) | Memory: 16.6 MB (32.99%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,9 +1,9 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        def cmp(x, y):
-            return -1 if x + y > y + x else 1
-    
         nums = [str(n) for n in nums]
-        nums.sort(key=cmp_to_key(cmp))
-        largest_num = ''.join(nums)
-        return '0' if largest_num[0] == '0' else largest_num
+
+        nums.sort(key=lambda x: x*10, reverse=True)
+
+        if nums[0] == '0': return '0'
+
+        return ''.join(nums)

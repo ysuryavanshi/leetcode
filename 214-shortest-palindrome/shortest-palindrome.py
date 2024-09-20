4bf12f33807f6a40ec20e9593c7e9e593b035ce7
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-09-20 09:32:32 -0700

    Time: 63 ms (69.65%) | Memory: 17 MB (41.65%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.py
+++ 214-shortest-palindrome/shortest-palindrome.py
@@ -3,13 +3,14 @@ class Solution:
         prefix, suffix, base = 0, 0, 29
         last_index = 0
         power = 1
+        mod = 10**9 + 7
 
         for i, c in enumerate(s):
             char = ord(c) - ord('a') + 1
-            prefix = ((prefix * base) + char)
+            prefix = ((prefix * base) % mod + char) % mod 
 
-            suffix = (suffix + char * power)
-            power = (power * base)
+            suffix = (suffix + char * power) % mod
+            power = (power * base) % mod
 
             if prefix == suffix:
                 last_index = i

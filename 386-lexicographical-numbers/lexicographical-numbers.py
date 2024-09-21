98f3dd685f4eeb3e872a2811027d13ee9667dfbb
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-09-20 21:43:29 -0700

    Time: 83 ms (37.20%) | Memory: 22.3 MB (91.75%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -1,5 +1,5 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
         nums = [i for i in range(1, n + 1)]
-        nums.sort(key=lambda x: str(x))
+        nums.sort(key=lambda x: x/10**(len(str(x))-1))
         return nums

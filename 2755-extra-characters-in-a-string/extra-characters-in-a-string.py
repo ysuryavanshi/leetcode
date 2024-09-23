e5c50ff98854dffd1801ec8d2196df34cbbb6553
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-09-23 15:59:08 -0700

    Time: 193 ms (67.07%) | Memory: 17.1 MB (35.19%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.py
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.py
@@ -1,36 +1,18 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.word = False
-
-class Trie:
-    def __init__(self, words):
-        self.root = TrieNode()
-        for word in words:
-            curr = self.root
-            for c in word:
-                if c not in curr.children:
-                    curr.children[c] = TrieNode()
-                curr = curr.children[c]
-            curr.word = True
-
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        dictionary = set(dictionary)
         dp = {len(s): 0}
-        trie = Trie(dictionary).root
 
         def dfs(i):
             if i == len(s): return 0
             if i in dp: return dp[i]
-
-            res = 1 + dfs(i + 1)
-            curr = trie
+            
+            ans = 1 + dfs(i + 1)
             for j in range(i, len(s)):
-                if s[j] not in curr.children:
-                    break
-                curr = curr.children[s[j]]
-                if curr.word:
-                    res = min(res, dfs(j + 1))
-            dp[i] = res
-            return res
+                if s[i:j+1] in dictionary:
+                    ans = min(ans, dfs(j+1))
+
+            dp[i] = ans
+            return ans
+        
         return dfs(0)

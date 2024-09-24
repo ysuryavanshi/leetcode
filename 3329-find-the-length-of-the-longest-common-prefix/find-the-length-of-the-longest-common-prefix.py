7b58dbda83c0dca21c40fd0b72ffbd9cca40f0e2
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-09-24 08:00:31 -0700

    Time: 688 ms (94.30%) | Memory: 28.6 MB (70.69%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
@@ -1,40 +1,18 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.word = False
-    
-
-class Trie:
-    def __init__(self, numbers: str):
-        self.root = TrieNode()
-        for num in numbers:
-            cur = self.root
-            for d in num:
-                if d not in cur.children:
-                    cur.children[d] = TrieNode()
-                cur = cur.children[d]
-            cur.word = True
-
-
 class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
-        if len(arr2) < len(arr1):
-            arr1, arr2 = arr2, arr1
-
-        arr1 = [str(i) for i in arr1]
-        arr2 = [str(i) for i in arr2]
-
-        trie_arr1 = Trie(arr1).root
-
+        p = set()
+        for num in arr1:
+            while num:
+                p.add(num)
+                num //= 10
+        
         ans = 0
         for num in arr2:
-            m = 0
-            cur = trie_arr1
-            for d in num:
-                if d in cur.children:
-                    m += 1
-                    cur = cur.children[d]
-                else:
+            while num:
+                if num in p:
+                    ans = max(ans, len(str(num)))
                     break
-            ans = max(ans, m)
+                else:
+                    num //= 10
+        
         return ans

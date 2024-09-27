6b4bf2b28556ae6204587ce52f6709aefad9791c
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-09-27 07:28:19 -0700

    Time: 225 ms (97.14%) | Memory: 17.4 MB (45.71%) - LeetSync

--- 731-my-calendar-ii/my-calendar-ii.py
+++ 731-my-calendar-ii/my-calendar-ii.py
@@ -5,30 +5,19 @@ class MyCalendarTwo:
         self.non_overlapping = []
 
     def book(self, start: int, end: int) -> bool:
-        for s, e in self.overlapping:
-            if s < end and start < e:
-                return False
-
-        for s, e in self.non_overlapping:
-            if s < end and start < e:
-                self.overlapping.append((max(start, s), min(end, e)))
         
-        self.non_overlapping.append((start, end))
-        return True
-
-
-        self.calendar.add((start, 1))
-        self.calendar.add((end, -1))
-
-        total = 0
-        for i, j in self.calendar:
-            total += j
-
-            if total == 3:
-                self.calendar.remove((start, 1))
-                self.calendar.remove((end, -1))
+        for s, e in self.overlapping:
+            if start < e and end > s:
                 return False
+
+        for s, e in self.non_overlapping:        
+            if start < e and end > s:
+                self.overlapping.append((max(start, s), min(end, e)))
+        self.non_overlapping.append((start, end))
+
         return True
+        
+
 
 
 # Your MyCalendarTwo object will be instantiated and called as such:

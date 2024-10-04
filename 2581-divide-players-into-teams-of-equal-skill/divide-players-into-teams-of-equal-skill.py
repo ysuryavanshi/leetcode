c6bc80854dbda0934eea1ecd8d962d4e9acfd741
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-10-03 22:52:15 -0700

    Time: 413 ms (32.21%) | Memory: 29.4 MB (68.50%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
@@ -4,8 +4,9 @@ class Solution:
         i, j = 0, len(skill) - 1
 
         chemistry = 0
+        check = skill[0] + skill[-1]
         while i < j:
-            if skill[0] + skill[-1] !=  skill[i] + skill[j]: return -1
+            if check  !=  skill[i] + skill[j]: return -1
             chemistry += skill[i] * skill[j]
             i += 1
             j -= 1

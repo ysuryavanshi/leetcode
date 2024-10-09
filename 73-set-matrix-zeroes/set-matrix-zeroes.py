49b1eba75a03c8f2731d7792e262a9625b5cbb9b
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-10-09 10:30:42 -0700

    Time: 106 ms (37.42%) | Memory: 17.3 MB (89.01%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,18 +1,30 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        m, n = len(matrix), len(matrix[0])
-        rows = set()
-        cols = set()
 
-        for i in range(m):
-            for j in range(n):
-                if matrix[i][j] == 0:
-                    rows.add(i)
-                    cols.add(j)
+        m = len(matrix)
+        n = len(matrix[0])
+		
+        first_row_has_zero = False
+        first_col_has_zero = False
         
-        for i in range(m):
-            for j in range(n):
-                if i in rows or j in cols: matrix[i][j] = 0
+        for row in range(m):
+            for col in range(n):
+                if matrix[row][col] == 0:
+                    if row == 0:
+                        first_row_has_zero = True
+                    if col == 0:
+                        first_col_has_zero = True
+                    matrix[row][0] = matrix[0][col] = 0
+    
+        for row in range(1, m):
+            for col in range(1, n):
+                matrix[row][col] = 0 if matrix[0][col] == 0 or matrix[row][0] == 0 else matrix[row][col]
+        
+        if first_row_has_zero:
+            for col in range(n):
+                matrix[0][col] = 0
+        
+        if first_col_has_zero:
+            for row in range(m):
+                matrix[row][0] = 0
+        

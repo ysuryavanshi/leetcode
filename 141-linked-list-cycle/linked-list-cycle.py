4703f72e369b606d58ef59c1ad73843e5ea604fd
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-10-14 10:26:12 -0700

    Time: 43 ms (70.87%) | Memory: 19.1 MB (70.38%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -6,11 +6,12 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        count = 0
-        while head != None and count < 1250:
-            try:
-                head = head.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next
-            except:
-                return False
-            count += 1
-        return count == 1250
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False

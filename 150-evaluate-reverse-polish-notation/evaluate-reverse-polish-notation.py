6a12e1633ec3a98693f20e0225d681f1382b1299
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-10-14 09:38:02 -0700

    Time: 70 ms (28.50%) | Memory: 17 MB (97.16%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,13 +1,16 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
+        def fsum(a, b): return a + b
+        def fsub(a, b): return a - b
+        def fmul(a, b): return a * b
+        def fdiv(a, b): x = a/b; return ceil(x) if x < 0 else floor(x)
+
         stack = []
-        operators = ['+', '-', '*', '/']
+        operators = {'+': fsum, '-': fsub, '*': fmul, '/': fdiv}
+
         for token in tokens:
             if token in operators:
                 op2, op1 = stack.pop(), stack.pop()
-                val = eval('%s%s%s'%(op1, token, op2))
-                if token == '/':
-                    val = floor(val) if val > 0 else ceil(val)
-                stack.append(val)
-            else: stack.append(token)
-        return int(stack[-1])
+                stack.append(operators[token](op1, op2))
+            else: stack.append(int(token))
+        return stack[-1]

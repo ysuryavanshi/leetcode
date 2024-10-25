61a9708ea7d66191e1143ac5a9e6bec6fab5ffb7
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-10-24 21:19:21 -0700

    Time: 23 ms (95.00%) | Memory: 29.4 MB (99.31%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -6,23 +6,12 @@ class Trie:
 
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
-        root = Trie()
+        folder.sort()
 
-        for path in folder:
-            node = root
-            for c in path.split('/'):
-                if c not in node.children:
-                    node.children[c] = Trie()
-                node = node.children[c]
-            node.end = True
+        ans = [folder[0]]
 
-        def backtrack(node, path):
-            if node.end:
-                return ['/'.join(path)]
-            ans = []
-            for c in node.children:
-                path.append(c)
-                ans += backtrack(node.children[c], path)
-                path.pop()
-            return ans
-        return backtrack(root, [])
+        for i in range(1, len(folder)):
+            last_folder = ans[-1] + '/'
+            if not folder[i].startswith(last_folder):
+                ans.append(folder[i])
+        return ans

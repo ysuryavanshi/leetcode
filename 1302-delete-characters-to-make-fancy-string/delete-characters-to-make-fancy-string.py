3e5e3b784a8cceb911daaf919dfa58e3dd643f29
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-11-01 11:31:16 -0700

    Time: 1035 ms (9.01%) | Memory: 18.6 MB (55.17%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -4,7 +4,8 @@ class Solution:
 
         stack = [s[0], s[1]]
         for c in s[2:]:
-            if stack[-2] != c or stack[-1] != c:
+            if set(stack[-2:]) != set(c):
+            # if stack[-2] != c or stack[-1] != c:
                 stack.append(c)
         return ''.join(stack)
                     

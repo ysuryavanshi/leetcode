880aa382b87badde01d6d1f95362f596333ec6f9
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-11-02 10:26:57 -0700

    Time: 19 ms (86.77%) | Memory: 16.9 MB (24.40%) - LeetSync

--- 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.py
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.py
@@ -1,29 +1,30 @@
-class Solution(object):
-    def minimumMountainRemovals(self, nums):
+class Solution:
+    def minimumMountainRemovals(self, nums: List[int]) -> int:
         n = len(nums)
-        
-        lis = [1] * n
-        temp = []
+        left = [0] * n
+        right = [0] * n
+
+        dp = []
         for i in range(n):
-            pos = bisect_left(temp, nums[i])
-            if pos < len(temp):
-                temp[pos] = nums[i]
+            idx = bisect_left(dp, nums[i])
+            if idx == len(dp):
+                dp.append(nums[i])
             else:
-                temp.append(nums[i])
-            lis[i] = pos + 1
-        lds = [1] * n
-        temp = []
+                dp[idx] = nums[i]
+            left[i] = len(dp)
+        
+        dp = []
         for i in range(n - 1, -1, -1):
-            pos = bisect_left(temp, nums[i])
-            if pos < len(temp):
-                temp[pos] = nums[i]
+            idx = bisect_left(dp, nums[i])
+            if idx == len(dp):
+                dp.append(nums[i])
             else:
-                temp.append(nums[i])
-            lds[i] = pos + 1
-
-        max_mountain_len = 0
-        for i in range(1, n - 1):
-            if lis[i] > 1 and lds[i] > 1: 
-                max_mountain_len = max(max_mountain_len, lis[i] + lds[i] - 1)
-
-        return n - max_mountain_len
+                dp[idx] = nums[i]
+            right[i] = len(dp)
+        
+        min_remove = n
+        for i in range(n):
+            if left[i] > 1 and right[i] > 1:
+                min_remove = min(min_remove, n - left[i] - right[i] + 1)
+        
+        return min_remove

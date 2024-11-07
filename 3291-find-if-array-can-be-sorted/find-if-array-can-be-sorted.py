e72721d04904178dad3de9b2f9e73dda1a8a690c
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-11-06 16:36:37 -0800

    Time: 3 ms (94.69%) | Memory: 16.6 MB (42.67%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
@@ -1,9 +1,16 @@
 class Solution:
     def canSortArray(self, nums: List[int]) -> bool:
-        n = len(nums)
-
-        for _ in range(n):
-            for i in range(n - 1):
-                if nums[i].bit_count() == nums[i + 1].bit_count() and nums[i + 1] < nums[i]:
-                    nums[i], nums[i + 1] = nums[i + 1], nums[i]
-        return nums == sorted(nums)
+        prev_max = cur_min = cur_max = 0
+        prev_bit = -1
+        for n in nums:
+            cur_bit = n.bit_count()
+            if cur_bit != prev_bit:
+                if cur_min < prev_max:
+                    return False
+                prev_max = cur_max
+                cur_min = cur_max = n
+                prev_bit = cur_bit
+            else:
+                cur_min = min(cur_min, n)
+                cur_max = max(cur_max, n)
+        return cur_min >= prev_max

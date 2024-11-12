04a0de6f1bc60fe944a1a8e3df127e1626b471cf
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-11-11 17:25:33 -0800

    Time: 175 ms (62.20%) | Memory: 63 MB (93.01%) - LeetSync

---
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
+        items.sort()
+
+        max_b = 0
+        for item in items:
+            max_b = max(item[1], max_b)
+            item[1] = max_b
+
+        ans = [0] * len(queries)
+        for i, q in enumerate(queries):
+            idx = bisect_right(items, [q, float('inf')])
+            if idx == 0: ans[i] = 0
+            elif idx == len(items):
+                ans[i] = items[-1][1]
+            else:
+                ans[i] = max(ans[i], items[idx - 1][1])
+        return ans

2d5f0d16dd857f2ad94206c3116d6c973c7cc45c
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-11-12 10:28:59 -0800

    Time: 126 ms (75.10%) | Memory: 77 MB (96.52%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,50 +1,24 @@
 class LRUCache:
-    class Node:
-        def __init__(self, key, val):
-            self.key = key
-            self.val = val
-            self.prev = None
-            self.next = None
 
     def __init__(self, capacity: int):
-        self.cap = capacity
-        self.head = self.Node(-1, -1)
-        self.tail = self.Node(-1, -1)
-        self.head.next = self.tail
-        self.tail.prev = self.head
-        self.m = {}
-
-    def add_node(self, new_node):
-        self.head.next, new_node.next = new_node, self.head.next
-        new_node.prev = self.head
-        new_node.next.prev = new_node
-    
-    def del_node(self, node):
-        node.next.prev = node.prev
-        node.prev.next = node.next
+        self.data = OrderedDict()
+        self.capacity = capacity
+        
 
     def get(self, key: int) -> int:
-        if key in self.m:
-            node = self.m[key]
-            ans = node.val
-            del self.m[key]
-            self.del_node(node)
-            self.add_node(node)
-            self.m[key] = self.head.next
-            return ans
+        if key in self.data:
+            return self.data.setdefault(key, self.data.pop(key))
         return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.m:
-            cur = self.m[key]
-            del self.m[key]
-            self.del_node(cur)
-        elif len(self.m) == self.cap:
-            del self.m[self.tail.prev.key]
-            self.del_node(self.tail.prev)
-        
-        self.add_node(self.Node(key, value))
-        self.m[key] = self.head.next
+        if key in self.data:
+            self.data.move_to_end(key)
+            self.data[key] = value
+        else:
+            self.data[key] = value
+            if len(self.data) > self.capacity:
+                self.data.popitem(last=False)
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)

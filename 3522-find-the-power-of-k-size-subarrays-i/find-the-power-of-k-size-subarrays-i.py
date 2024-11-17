a4bb7edb0d8faf53e2994e945b3de5afd8b21914
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-11-17 09:49:31 -0800

    Time: 15 ms (34.21%) | Memory: 16.7 MB (56.34%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
@@ -1,17 +1,17 @@
 class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
-        res = []
-        l = 0
-        consec_cnt = 1
-        for r in range(len(nums)):
-            if r > 0 and nums[r - 1] + 1 == nums[r]:
-                consec_cnt += 1
+        n = len(nums)
+        longest_inc = [(1, nums[0])]
 
-            if r - l + 1 > k:
-                if nums[l] + 1 == nums[l + 1]:
-                    consec_cnt -= 1
-                l += 1
-
-            if r - l + 1 == k:
-                res.append(nums[r] if consec_cnt == k else -1)
-        return res
+        for i in range(1, n):
+            if nums[i - 1] + 1 == nums[i]:
+                last = longest_inc[-1]
+                longest_inc.append((last[0] + 1, max(nums[i], last[1])))
+            else:
+                longest_inc.append((1, nums[i]))
+        
+        ans = []
+        for i in range(k - 1, n):
+            if longest_inc[i][0] >= k: ans.append(longest_inc[i][1])
+            else: ans.append(-1)
+        return ans

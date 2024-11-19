352c518b5a689c4b83e4a3029221146bb6773c97
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-11-18 18:21:47 -0800

    Time: 77 ms (99.76%) | Memory: 33.7 MB (77.58%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -2,28 +2,24 @@ class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
         n = len(nums)
         elements = set()
-        current_sum = 0
-        max_sum = 0
-        begin = 0
+        left = cur_sum = ans = 0
 
-        for end in range(n): 
-            if nums[end] not in elements:
-                current_sum += nums[end]
-                elements.add(nums[end])
+        for right in range(n):
+            if nums[right] not in elements:
+                cur_sum += nums[right]
+                elements.add(nums[right])
 
-                if end - begin + 1 == k:
-                    if current_sum > max_sum:
-                        max_sum = current_sum
-                    
-                    current_sum -= nums[begin]
-                    elements.remove(nums[begin])
-                    begin += 1
-            else:
-                while nums[begin] != nums[end]:
-                    current_sum -= nums[begin]
-                    elements.remove(nums[begin])
-                    begin += 1
+                if right - left + 1 == k:
+                    if cur_sum > ans:
+                        ans = cur_sum
                 
-                begin += 1
-
-        return max_sum
+                    cur_sum -= nums[left]
+                    elements.remove(nums[left])
+                    left += 1
+            else:
+                while nums[left] != nums[right]:
+                    cur_sum -= nums[left]
+                    elements.remove(nums[left])
+                    left += 1
+                left += 1
+        return ans

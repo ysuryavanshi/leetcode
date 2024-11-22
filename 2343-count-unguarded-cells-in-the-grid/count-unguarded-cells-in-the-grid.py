bd20e324350719804c9c06a6fb0d59f93fd49b37
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-11-22 10:54:56 -0800

    Time: 458 ms (45.45%) | Memory: 38.1 MB (85.00%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.py
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.py
@@ -1,17 +1,22 @@
 class Solution:
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
-        grid = [[0] * n for _ in range(m)] # Initialize grid with zeros
-        # Mark obstacles
-        for x, y in guards + walls: # gx,gy - Guards/Walls position
-            grid[x][y] = 2  
-        # Process guards' vision
-        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-        for gx, gy in guards:
-            for dx, dy in directions: # dx, dy - movement direction offsets
-                x, y = gx, gy
-                # Check cells in current direction until hitting boundary or obstacle
-                while 0 <= x + dx < m and 0 <= y + dy < n and grid[x + dx][y + dy] != 2:
-                    x += dx
-                    y += dy
-                    grid[x][y] = 1 
-        return sum(row.count(0) for row in grid) # Count unguarded cells (value 0)
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+        ans = [[-1] * n for _ in range(m)]
+
+        for w in walls:
+            ans[w[0]][w[1]] = 'W'
+
+        for g in guards:
+            ans[g[0]][g[1]] = 'G'
+        
+        for g in guards:
+            for d in directions:
+                cur = [g[0] + d[0], g[1] + d[1]]
+                while 0 <= cur[0] < m and 0 <= cur[1] < n:
+                    if ans[cur[0]][cur[1]] in [-1, 1]:
+                        ans[cur[0]][cur[1]] = 1
+                        cur = [cur[0] + d[0], cur[1] + d[1]]
+                    else:
+                        break
+
+        return sum([sum([1 for c in r if c == -1]) for r in ans])

034d1e16aa29bc9f9d17d6ff39be95ce6d854544
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-11-22 11:01:06 -0800

    Time: 310 ms (86.36%) | Memory: 38.6 MB (67.22%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.py
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.py
@@ -1,22 +1,17 @@
 class Solution:
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
         directions = [(1,0), (-1,0), (0,1), (0,-1)]
-        ans = [[-1] * n for _ in range(m)]
+        ans = [[0] * n for _ in range(m)]
 
-        for w in walls:
-            ans[w[0]][w[1]] = 'W'
+        for x, y in walls + guards:
+            ans[x][y] = 2
 
-        for g in guards:
-            ans[g[0]][g[1]] = 'G'
-        
-        for g in guards:
-            for d in directions:
-                cur = [g[0] + d[0], g[1] + d[1]]
-                while 0 <= cur[0] < m and 0 <= cur[1] < n:
-                    if ans[cur[0]][cur[1]] in [-1, 1]:
-                        ans[cur[0]][cur[1]] = 1
-                        cur = [cur[0] + d[0], cur[1] + d[1]]
-                    else:
-                        break
+        for gx, gy in guards:
+            for dx, dy in directions:
+                x, y = gx, gy
+                while 0 <= x + dx < m and 0 <= y + dy < n and ans[x + dx][y + dy] != 2:
+                    x += dx
+                    y += dy
+                    ans[x][y] = 1
 
-        return sum([sum([1 for c in r if c == -1]) for r in ans])
+        return sum(row.count(0) for row in ans)

8a239fcee116de15203f563ec0e614b65512caab
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-11-23 19:55:05 -0800

    Time: 59 ms (89.94%) | Memory: 25 MB (41.30%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.py
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.py
@@ -8,7 +8,8 @@ class Solution:
                 if matrix[i][j] < 0:
                     count_negative += 1
                 
-                min_value = min(abs(matrix[i][j]), min_value)
+                if abs(matrix[i][j]) < min_value:
+                    min_value = abs(matrix[i][j])
                 ans += abs(matrix[i][j])
         
         return ans - (2 * min_value) if count_negative % 2 == 1 else ans

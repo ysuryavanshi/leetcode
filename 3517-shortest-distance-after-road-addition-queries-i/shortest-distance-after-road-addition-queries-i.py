20d37069c3383e8ab448b0d76337516215ad6e71
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-11-27 12:05:29 -0800

    Time: 472 ms (68.64%) | Memory: 17.6 MB (8.40%) - LeetSync

--- 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.py
+++ 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.py
@@ -1,6 +1,6 @@
 class Solution:
     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
-        graph = [[] for i in range(n)]
+        graph = [[i + 1] for i in range(n)]
 
         def get_distance():
             q = deque([(0, 0)])
@@ -11,10 +11,6 @@ class Solution:
                 if cur == n - 1:
                     return distance
                 else:
-                    nxt = cur + 1
-                    if nxt not in visited:
-                            q.append((nxt, distance + 1))
-                            visited.add(nxt)
                     for nxt in graph[cur]:
                         if nxt not in visited:
                             q.append((nxt, distance + 1))

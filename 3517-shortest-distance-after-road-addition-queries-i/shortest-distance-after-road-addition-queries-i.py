a2f81f04fc9a9a85207acae4f3be2f0104510149
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-11-27 12:02:35 -0800

    Time: 464 ms (70.11%) | Memory: 17.8 MB (8.40%) - LeetSync

--- 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.py
+++ 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.py
@@ -1,23 +1,23 @@
 class Solution:
     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
-        adj = [[i + 1] for i in range(n)]
+        graph = [[i + 1] for i in range(n)]
 
-        def shortest_path():
-            q = deque()
-            q.append((0,0))
-            visit = set()
-            visit.add((0,0))
+        def get_distance():
+            q = deque([(0, 0)])
+            visited = set()
+            visited.add(0)
             while q:
                 cur, distance = q.popleft()
-                if cur == n-1:
+                if cur == n - 1:
                     return distance
-                for neigh in adj[cur]:
-                    if neigh not in visit:
-                        q.append((neigh, distance + 1))
-                        visit.add(neigh)
+                else:
+                    for nxt in graph[cur]:
+                        if nxt not in visited:
+                            q.append((nxt, distance + 1))
+                            visited.add(nxt)
 
         res = []
         for src, dst in queries:
-            adj[src].append(dst)
-            res.append(shortest_path())
+            graph[src].append(dst)
+            res.append(get_distance())
         return res

6303d7118a06099af364d150be1b322a543bb3f9
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-11-29 15:52:53 -0800

    Time: 878 ms (63.44%) | Memory: 43.4 MB (17.65%) - LeetSync

--- 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.py
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.py
@@ -1,37 +1,34 @@
 class Solution:
     def minimumTime(self, grid: List[List[int]]) -> int:
-        if grid[1][0] > 1 and grid[0][1] > 1:
+        if grid[0][1] > 1 and grid[1][0] > 1:
             return -1
-        R, C = len(grid), len(grid[0])
+        
+        m, n = len(grid), len(grid[0])
+        min_heap = [(0,0,0)] # cost, x, y
+        heapify(min_heap)
+        visited = set()
 
-        def isOutside(i, j):
-            return i < 0 or i >= R or j < 0 or j >= C
+        while min_heap:
+            cost, x, y = heapq.heappop(min_heap)
+            if x == m - 1 and y == n - 1:
+                return cost
 
-        def idx(i, j):
-            return i * C + j
+            if x + 1 < m and (x + 1, y) not in visited:
+                c = max(cost + 1, grid[x+1][y] + (grid[x+1][y] - cost + 1) % 2)
+                heapq.heappush(min_heap, (c, x + 1, y))
+                visited.add((x + 1, y))
 
-        N = R * C
-        time = [2**31] * N
-        pq = [0]
+            if y + 1 < n and (x, y + 1) not in visited:
+                c = max(cost + 1, grid[x][y+1] + (grid[x][y+1] - cost + 1) % 2)
+                heapq.heappush(min_heap, (c, x, y+1))
+                visited.add((x, y + 1))
 
-        time[0] = 0
-        while len(pq):
-            tij = heappop(pq)
-            t, ij = tij >> 32, tij & ((1 << 30) - 1)
-            i, j = divmod(ij, C)
-            if i == R - 1 and j == C - 1:
-                return t
+            if x - 1 >= 0 and (x - 1, y) not in visited:
+                c = max(cost + 1, grid[x-1][y] + (grid[x-1][y] - cost + 1) % 2)
+                heapq.heappush(min_heap, (c, x - 1, y))
+                visited.add((x - 1, y))
 
-            for di, dj in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-                r, s = i + di, j + dj
-                if isOutside(r, s):
-                    continue
-
-                w = 0 if (grid[r][s] - t) & 1 else 1
-                nextTime = max(t + 1, grid[r][s] + w)
-
-                rs = idx(r, s)
-                if nextTime < time[rs]:
-                    time[rs] = nextTime
-                    heappush(pq, (nextTime << 32) + rs)
-        return -1
+            if y - 1 >= 0 and (x, y - 1) not in visited:
+                c = max(cost + 1, grid[x][y-1] + (grid[x][y-1] - cost + 1) % 2)
+                heapq.heappush(min_heap, (c, x, y-1))
+                visited.add((x, y - 1))

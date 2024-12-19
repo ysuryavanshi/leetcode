1059c9c9441e43d5fd9545a3cc380569f9a770b8
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-12-19 12:29:46 -0800

    Time: 229 ms (58.72%) | Memory: 19.1 MB (82.99%) - LeetSync

--- 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.py
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.py
@@ -4,22 +4,22 @@ class Solution:
         pq = [(-ord(char), count) for char, count in freq.items()]
         heapq.heapify(pq)
 
-        result = []
+        result = ''
         while pq:
             ch, count = heapq.heappop(pq)
             ch = chr(-ch)
             used = min(repeatLimit, count)
-            result.append(ch * used)
+            result += ch * used
             count -= used
 
             if count > 0:
                 if not pq: break
                 next_ch, next_count = heapq.heappop(pq)
-                result.append(chr(-next_ch))
+                result += chr(-next_ch)
                 next_count -= 1
 
                 if next_count > 0:
                     heapq.heappush(pq, (next_ch, next_count))
                 heapq.heappush(pq, (-ord(ch), count))
         
-        return "".join(result)
+        return result

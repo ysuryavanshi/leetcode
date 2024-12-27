645f348925f0d8a33eacae7d462e2bd3d4d1baa8
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-12-27 08:28:49 -0800

    Time: 70 ms (52.44%) | Memory: 23.3 MB (20.49%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.py
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.py
@@ -1,18 +1,11 @@
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
         n = len(values)
-        suffixMax = [0] * n
-        suffixMax[n - 1] = values[n - 1] - (n - 1)
-
-        for i in range(n - 2, -1, -1):
-            suffixMax[i] = max(suffixMax[i + 1], values[i] - i)
-
-        maxScore = float('-inf')
-
-        for i in range(n - 1):
-            maxScore = max(maxScore, values[i] + i + suffixMax[i + 1])
-
-        return maxScore
+        dp = res = 0
+        for idx, val in enumerate(values):
+            res = max(res, dp + val - idx)
+            dp = max(dp, val + idx)
+        return res
 
 # class Solution:
 #     def maxScoreSightseeingPair(self, values: List[int]) -> int:

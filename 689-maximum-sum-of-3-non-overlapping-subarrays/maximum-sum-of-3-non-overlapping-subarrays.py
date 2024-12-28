2ca696659c6c9fb29b819574dfbe120773d4a494
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2024-12-28 13:31:17 -0800

    Time: 147 ms (19.45%) | Memory: 26.1 MB (24.93%) - LeetSync

--- 689-maximum-sum-of-3-non-overlapping-subarrays/maximum-sum-of-3-non-overlapping-subarrays.py
+++ 689-maximum-sum-of-3-non-overlapping-subarrays/maximum-sum-of-3-non-overlapping-subarrays.py
@@ -1,37 +1,50 @@
 class Solution:
-    def maxSumOfThreeSubarrays(self, nums: List[int], k: int) -> List[int]:
+    def __init__(self):
+        self.prefix_sum = []
+        self.mem = []
+
+    def find_max_sum(self, nums, pos, count, k):
+        if count == 3:
+            return 0
+        if pos > len(nums) - k:
+            return 0
+        if self.mem[pos][count] != -1:
+            return self.mem[pos][count]
+
+        dont_start = self.find_max_sum(nums, pos + 1, count, k)
+
+        start_here = self.find_max_sum(nums, pos + k, count + 1, k) + self.prefix_sum[pos + k] - self.prefix_sum[pos]
+
+        self.mem[pos][count] = max(dont_start, start_here)
+        return self.mem[pos][count]
+
+    def find_max_sum_path(self, nums, pos, count, k, path):
+        if count == 3:
+            return
+        if pos > len(nums) - k:
+            return
+
+        dont_start = self.find_max_sum(nums, pos + 1, count, k)
+
+        start_here = self.find_max_sum(nums, pos + k, count + 1, k) + self.prefix_sum[pos + k] - self.prefix_sum[pos]
+
+        if start_here >= dont_start:
+            path.append(pos)
+            self.find_max_sum_path(nums, pos + k, count + 1, k, path)
+        else:
+            self.find_max_sum_path(nums, pos + 1, count, k, path)
+
+    def maxSumOfThreeSubarrays(self, nums, k):
         n = len(nums)
-        p_sum = [sum(nums[:k])]
-        for i in range(k, n):
-            p_sum.append(p_sum[-1] + nums[i] - nums[i - k])
+        self.mem = [[-1] * 3 for _ in range(n)]
 
-        dp = {}
-        def helper(i, count_s_arr):
-            if count_s_arr == 3 or i > n - k:
-                return 0
-            if (i, count_s_arr) in dp:
-                return dp[(i, count_s_arr)]
+        self.prefix_sum = [0] * (n + 1)
+        for i in range(n):
+            self.prefix_sum[i + 1] = self.prefix_sum[i] + nums[i]
 
-            skip = helper(i + 1, count_s_arr)
-            include = p_sum[i] + helper(i + k, count_s_arr + 1)
+        self.find_max_sum(nums, 0, 0, k)
 
-            dp[(i, count_s_arr)] = max(skip, include)
+        path = []
+        self.find_max_sum_path(nums, 0, 0, k, path)
 
-            return dp[(i, count_s_arr)]
-
-        def get_index():
-            i = 0
-            res = []
-
-            while i <= n - k and len(res) < 3:
-                include = p_sum[i] + helper(i + k, len(res) + 1)
-                skip = helper(i + 1, len(res))
-
-                if include >= skip:
-                    res.append(i)
-                    i += k
-                else:
-                    i += 1
-            return res
-
-        return get_index()
+        return path

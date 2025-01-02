8e0462657d0df69e360b02b30769c3bc1b21fd26
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2025-01-01 20:37:28 -0800

    Time: 15 ms (93.12%) | Memory: 49.7 MB (40.21%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.py
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.py
@@ -3,11 +3,11 @@ class Solution:
         p_count = [0]
         vowels = ('a', 'e', 'i', 'o', 'u')
 
+        cur = 0
         for word in words:
             if word[0] in vowels and word[-1] in vowels:
-                p_count.append(p_count[-1] + 1)
-            else:
-                p_count.append(p_count[-1])
+                cur += 1
+            p_count.append(cur)
         
         res = []
         for l, r in queries:

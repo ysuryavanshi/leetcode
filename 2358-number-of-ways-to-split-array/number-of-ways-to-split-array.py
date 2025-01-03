c3a57ad97f820d2a987ec69043a3463f765673a6
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2025-01-03 00:21:47 -0800

    Time: 39 ms (97.22%) | Memory: 33.5 MB (10.24%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
@@ -1,20 +1,14 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
         p_sum = [0]
-        s_sum = [0]
 
         cur = 0
         for n in nums:
             cur += n
             p_sum.append(cur)
         
-        cur = 0
-        for n in reversed(nums):
-            cur += n
-            s_sum.insert(0, cur)
-        
         res = 0
-        for x, y in zip(p_sum[1:-1], s_sum[1:-1]):
-            if x >= y: res += 1
-        
+        for n in p_sum[1:-1]:
+            if n >= cur - n:
+                res += 1
         return res

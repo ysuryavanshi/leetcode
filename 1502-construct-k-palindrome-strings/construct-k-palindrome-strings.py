c17ccdd6cf0e5401040a438b63e1407346060896
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2025-01-11 09:47:01 -0800

    Time: 23 ms (98.24%) | Memory: 18 MB (43.17%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -1,9 +1,13 @@
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
-        if len(s) < k: return False
-
-        count = Counter(s)
-        if sum([count[c] % 2 != 0 for c in count]) > k:
+        if len(s) == k:
+            return True
+        if len(s) < k:
             return False
-
+        odd = 0
+        for char in set(s):
+            if s.count(char) % 2:
+                odd += 1
+        if odd > k:
+            return False
         return True

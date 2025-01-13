47bf3179325455539f573af9136d9227c17b11b1
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2025-01-13 08:18:48 -0800

    Time: 64 ms (89.21%) | Memory: 18.7 MB (52.38%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -1,9 +1,14 @@
+from string import ascii_lowercase
+
 class Solution:
     def minimumLength(self, s: str) -> int:
-    	count = Counter(s)
-
-    	res = 0
-    	for _, value in count.items():
-    		res += 1 if value % 2 else 2
-
-    	return res
+        chars = set(ascii_lowercase)
+        
+        ans = 0
+        for c in chars:
+            count = s.count(c)
+            if count & 1:
+                ans += 1
+            elif count != 0:
+                ans += 2
+        return ans

de97936be245a9b6d0ed73da15ee73ae14c6abd6
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2025-01-15 08:03:57 -0800

    Time: 0 ms (100.00%) | Memory: 18 MB (14.40%) - LeetSync

--- 2509-minimize-xor/minimize-xor.py
+++ 2509-minimize-xor/minimize-xor.py
@@ -2,21 +2,15 @@ class Solution:
     def minimizeXor(self, num1: int, num2: int) -> int:
         c1, c2 = num1.bit_count(), num2.bit_count()
 
-        if c1 == c2:
-            return num1
-
         i = 0
         temp = num1
-        while c1 > c2:
-            if temp & (1 << i):
+        while c1 != c2:
+            if c1 > c2 and temp & (1 << i):
                 c1 -= 1
-                temp = temp ^ (1 << i)
-            i += 1
-        
-        while c1 < c2:
-            if temp & (1 << i) == 0:
+                temp ^= 1 << i
+            if c1 < c2 and temp & (1 << i) == 0:
                 c1 += 1
-                temp = temp | (1 << i)
+                temp |= 1 << i
             i += 1
 
         return temp

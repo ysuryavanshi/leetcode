9fa8a040715d3720fb0d2c8769c927162931e945
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2025-01-17 10:32:18 -0800

    Time: 28 ms (88.32%) | Memory: 22.4 MB (48.90%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.py
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.py
@@ -1,7 +1,3 @@
 class Solution:
     def doesValidArrayExist(self, derived: List[int]) -> bool:
-        first = last = 0
-        for num in derived:
-            if num == 1: last = ~last
-        
-        return False if first != last else True
+        return sum(derived) % 2 == 0

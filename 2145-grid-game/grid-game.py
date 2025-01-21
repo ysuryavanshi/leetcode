0a1a06442625428552de4fc64de71b8d95341a37
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2025-01-21 11:41:07 -0800

    Time: 91 ms (69.28%) | Memory: 29.8 MB (49.67%) - LeetSync

--- 2145-grid-game/grid-game.py
+++ 2145-grid-game/grid-game.py
@@ -1,17 +1,11 @@
 class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
-        prefix_sum = [[], []]
-        n = len(grid[0])
-
-        for i in range(2):
-            last = 0
-            for j in range(n):
-                last += grid[i][j]
-                prefix_sum[i].append(last)
-        
         res = float('inf')
-        for i in range(n):
-            sec1 = prefix_sum[0][-1] - prefix_sum[0][i]
-            sec2 = prefix_sum[1][i - 1] if i > 0 else 0
-            res = min(res, max(sec1, sec2))
+        sum_r1 = sum(grid[0])
+        sum_r2 = 0
+
+        for i in range(len(grid[0])):
+            sum_r1 -= grid[0][i]
+            res = min(res, max(sum_r1, sum_r2))
+            sum_r2 += grid[1][i]
         return res

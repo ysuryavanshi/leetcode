f908ff84ebf1159d828703a4617cc6a3e2cc6951
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2025-01-22 08:07:48 -0800

    Time: 615 ms (98.12%) | Memory: 84.9 MB (60.00%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.py
+++ 1876-map-of-highest-peak/map-of-highest-peak.py
@@ -2,14 +2,12 @@ class Solution:
     def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
         m, n = len(isWater), len(isWater[0])
         q = deque([])
-        visited = set()        
         res = [[-1] * n for _ in range(m)]
 
         for i in range(m):
             for j in range(n):
                 if isWater[i][j]:
                     q.append((i,j))
-                    visited.add((i,j))
                     res[i][j] = 0
 
         directions = [(0,1), (0,-1), (1,0), (-1,0)]
@@ -20,11 +18,10 @@ class Solution:
             for dx, dy in directions:
                 nr, nc = r + dx, c + dy
 
-                if nr < 0 or nc < 0 or nr == m or nc == n or (nr, nc) in visited:
+                if nr < 0 or nc < 0 or nr == m or nc == n or res[nr][nc] != -1:
                     continue
                 
                 q.append((nr, nc))
-                visited.add((nr, nc))
                 res[nr][nc] = val
         
         return res

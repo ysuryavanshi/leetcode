559b8fd64bacc7a365ddfb7410e23d53ba6add54
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2025-02-05 09:17:54 -0800

    Time: 1 ms (49.72%) | Memory: 19.5 MB (83.90%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -6,30 +6,16 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, node: Optional[TreeNode]) -> bool:
-        
-        def helper(node, min_v, max_v):
+        prev = float('-inf')
+
+        def inorder(node):
+            nonlocal prev
             if not node:
                 return True
-            if not min_v < node.val < max_v:
+            if not (inorder(node.left) and prev < node.val):
                 return False
-            
-            if node.right and node.left:
-                return (
-                    node.left.val < node.val < node.right.val and
-                    helper(node.left, min_v, node.val) and
-                    helper(node.right, node.val, max_v)
-                )
-            elif node.right:
-                return (
-                    node.val < node.right.val and
-                    helper(node.right, node.val, max_v)
-                )
-            elif node.left:
-                return (
-                    node.left.val < node.val and
-                    helper(node.left, min_v, node.val)
-                )
-            else:
-                return True
 
-        return helper(node, float('-inf'), float('inf'))
+            prev = node.val
+            return inorder(node.right)
+
+        return inorder(node)

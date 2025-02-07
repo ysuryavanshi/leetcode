d3b8ab738788436f30a946e312aa768de10f355d
Yash Suryavanshi <36504298+yashsxd@users.noreply.github.com>
2025-02-07 10:45:15 -0800

    Time: 47 ms (28.82%) | Memory: 18.1 MB (71.11%) - LeetSync

---
+++ 133-clone-graph/clone-graph.py
@@ -0,0 +1,26 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+class Solution:
+    def cloneGraph(self, root):
+        if not root:
+            return None
+        
+        all_nodes = {root.val: Node(root.val, [])}
+        q = deque([root])
+
+        while q:
+            node = q.popleft()
+            new_node = all_nodes[node.val]
+            
+            for nei in node.neighbors:
+                if nei.val not in all_nodes:
+                    all_nodes[nei.val] = Node(nei.val, [])
+                    q.append(nei)
+                
+                new_node.neighbors.append(all_nodes[nei.val])
+        return all_nodes[root.val]

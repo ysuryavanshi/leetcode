1356e9a2c32b26545d63512100bdf49be04e9a78
Yash Suryavanshi <36504298+ysuryavanshi@users.noreply.github.com>
2025-02-19 19:09:45 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (68.34%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,25 +1,27 @@
 class Solution:
-    def letterCombinations(self, digits: str) -> list[str]:
-        if not digits: return []
-
-        phone_map = {
-            '2': 'abc',
-            '3': 'def',
-            '4': 'ghi',
-            '5': 'jkl',
-            '6': 'mno',
-            '7': 'pqrs',
-            '8': 'tuv',
-            '9': 'wxyz'
+    def letterCombinations(self, digits: str) -> List[str]:
+        keyboard = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
         }
+        res = []
 
-        def backtrack(combo, n_digits):
-            if not n_digits:
-                ans.append(combo)
-            else:
-                for l in phone_map[n_digits[0]]:
-                    backtrack(combo + l, n_digits[1:])
-        
-        ans = []
-        backtrack('', digits)
-        return ans
+        def backtrack(i, chars):
+            nonlocal res
+            if i == len(digits):
+                if chars:
+                    res.append(chars)
+                return
+            
+            for letters in keyboard[digits[i]]:
+                for l in letters:
+                    backtrack(i+1, chars + l)
+
+        backtrack(0, '')
+        return res

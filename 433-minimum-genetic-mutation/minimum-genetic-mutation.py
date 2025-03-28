ee3069ed863cce805b6101f265afc78e00c9f7d4
Yash Suryavanshi <36504298+ysuryavanshi@users.noreply.github.com>
2025-03-28 14:47:19 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (38.88%) - LeetSync

---
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
+        bank = set(bank)
+        if endGene not in bank:
+            return -1
+
+        q = deque([startGene])
+        res = 0
+        while q:
+            res += 1
+            for _ in range(len(q)):
+                gene = list(q.popleft())
+                for i, temp in enumerate(gene):
+                    for c in 'ACGT':
+                        if gene[i] == c:
+                            continue
+                        
+                        gene[i] = c
+                        gene_str = ''.join(gene)
+                        if gene_str == endGene:
+                            return res
+                        elif gene_str in bank:
+                            q.append(gene_str)
+                            bank.remove(gene_str)
+                    gene[i] = temp
+        
+        return -1

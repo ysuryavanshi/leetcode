930ec15e4c68616287450167b61eded1dd80f79f
Yash Suryavanshi <36504298+ysuryavanshi@users.noreply.github.com>
2025-03-31 00:30:33 -0700

    Time: 129 ms (68.18%) | Memory: 30.2 MB (86.78%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.py
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.py
@@ -3,7 +3,6 @@ class Solution:
         if k in [len(weights), 1]:
             return 0
 
-        splits = [weights[i - 1] + weights[i] for i in range(1, len(weights))]
-        splits.sort()
+        splits = sorted([weights[i - 1] + weights[i] for i in range(1, len(weights))])
         
         return sum(splits[-k + 1:]) - sum(splits[:k - 1])

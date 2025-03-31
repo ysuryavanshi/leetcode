85814e46e9af947389ab961534af26fff5f8b8ec
Yash Suryavanshi <36504298+ysuryavanshi@users.noreply.github.com>
2025-03-31 00:28:26 -0700

    Time: 121 ms (90.91%) | Memory: 30.2 MB (86.78%) - LeetSync

---
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.py
@@ -0,0 +1,9 @@
+class Solution:
+    def putMarbles(self, weights: List[int], k: int) -> int:
+        if k == 1:
+            return 0
+
+        splits = [weights[i - 1] + weights[i] for i in range(1, len(weights))]
+        
+        splits.sort()
+        return sum(splits[-k + 1:]) - sum(splits[:k - 1])

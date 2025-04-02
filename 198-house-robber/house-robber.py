40713e434d783de28e380d25c21605195687af2d
Yash Suryavanshi <36504298+ysuryavanshi@users.noreply.github.com>
2025-04-02 09:50:29 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (13.00%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,11 +1,16 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
+        dp = [0] * n
         
         @cache
         def helper(i):
             if i >= n:
                 return 0
-            return max(nums[i] + helper(i + 2), helper(i + 1))
+            if dp[i] != 0:
+                return dp[i]
+
+            dp[i] = max(nums[i] + helper(i + 2), helper(i + 1))
+            return dp[i]
         
         return helper(0)

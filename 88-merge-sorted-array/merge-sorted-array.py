0f4a2c933ac7dfbb3918bec11094d885f1638b0e
Yash Suryavanshi <36504298+ysuryavanshi@users.noreply.github.com>
2025-04-12 18:25:44 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (93.79%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,13 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        a, b, write_index = m - 1, n - 1, m + n - 1
+        insert_index = m + n - 1
+        m -= 1
+        n -= 1
 
-        while b >= 0:
-            if a >= 0 and nums1[a] > nums2[b]:
-                nums1[write_index] = nums1[a]
-                a -= 1
+        while m >= 0 and n >= 0:
+            if nums1[m] > nums2[n]:
+                nums1[insert_index] = nums1[m]
+                m -= 1
             else:
-                nums1[write_index] = nums2[b]
-                b -= 1
-            write_index -= 1
+                nums1[insert_index] = nums2[n]
+                n -= 1
+            insert_index -= 1
+        
+        while n >= 0:
+            nums1[insert_index] = nums2[n]
+            insert_index -= 1
+            n -= 1

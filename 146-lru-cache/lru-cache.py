85c8a9d789bb8395ee1bcd8851030b7196187ab4
Yash Suryavanshi <36504298+ysuryavanshi@users.noreply.github.com>
2025-04-13 11:16:07 -0700

    Time: 149 ms (47.39%) | Memory: 78.2 MB (76.64%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,23 +1,64 @@
+class Node:
+    def __init__(self, key, val, previous=None, nxt=None):
+        self.key = key
+        self.val = val
+        self.previous = previous
+        self.next = nxt
+
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.data = OrderedDict()
-        self.capacity = capacity
-        
+        self.max_capacity = capacity
+        self.cur_capacity = 0
+        self.lookup = dict()
+
+        self.head = Node(None, None)
+        self.tail = Node(None, None)
+        self.head.next = self.tail
+        self.tail.previous = self.head
 
     def get(self, key: int) -> int:
-        if key in self.data:
-            return self.data.setdefault(key, self.data.pop(key))
-        return -1
+        if key not in self.lookup:
+            return -1
+        
+        node = self.lookup[key]
+        node.previous.next = node.next
+        node.next.previous = node.previous
+
+        node.previous = self.head
+        node.next = self.head.next
+        self.head.next = node
+        node.next.previous = node
+        
+        return node.val
+    
+    def evict(self):
+        del self.lookup[self.tail.previous.key]
+        self.cur_capacity -= 1
+
+        node = self.tail.previous
+        node.previous.next = self.tail
+        self.tail.previous = node.previous
+        
+        node.previous = node.next = None
 
     def put(self, key: int, value: int) -> None:
-        if key in self.data:
-            self.data.move_to_end(key)
-            self.data[key] = value
-        else:
-            self.data[key] = value
-            if len(self.data) > self.capacity:
-                self.data.popitem(last=False)
+        if key in self.lookup:
+            _ = self.get(key)
+            self.lookup[key].val = value
+            return None
+
+        if self.cur_capacity == self.max_capacity:
+            self.evict()
+
+        node = Node(key, value, self.head, self.head.next)
+        self.lookup[key] = node
+        self.head.next.previous = node
+        self.head.next = node
+        self.cur_capacity += 1
+
+        return None
 
 
 # Your LRUCache object will be instantiated and called as such:

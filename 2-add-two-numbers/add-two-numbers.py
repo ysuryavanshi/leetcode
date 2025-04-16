5d65758406ffb392a4484c66dfe93da82cd5599c
Yash Suryavanshi <36504298+ysuryavanshi@users.noreply.github.com>
2025-04-16 12:23:01 -0700

    Time: 2 ms (77.94%) | Memory: 18 MB (36.20%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -1,27 +1,46 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        root = ListNode()
+        head = root
         carry = 0
-        root = node = ListNode()
-        while l1 or l2 or carry:
-            v1 = v2 = 0
-            if l1:
-                v1 = l1.val
-                l1 = l1.next
-            if l2:
-                v2 = l2.val
-                l2 = l2.next
-            carry, val = divmod(v1 + v2 + carry, 10)
-            node.next = ListNode(val)
-            node = node.next
-        return root.next
 
+        while l1 and l2:
+            total = l1.val + l2.val + carry
+            carry = total // 10
+            total = total % 10
+            
+            head.next = ListNode(total)
+            head = head.next
+            
+            l1 = l1.next
+            l2 = l2.next
+
+        while l1:
+            total = l1.val + carry
+            carry = total // 10
+            total %= 10
+
+            head.next = ListNode(total)
+            head = head.next
+
+            l1 = l1.next
+        
+        while l2:
+            total = l2.val + carry
+            carry = total // 10
+            total %= 10
+
+            head.next = ListNode(total)
+            head = head.next
+
+            l2 = l2.next
+
+        if carry:
+            head.next = ListNode(1)
+        
+        return root.next

cc48613c63af09852a011286fec4198f480f2713
Yash Suryavanshi <36504298+ysuryavanshi@users.noreply.github.com>
2025-04-17 13:21:31 -0700

    Time: 1 ms (89.84%) | Memory: 17.8 MB (71.89%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -3,14 +3,12 @@ class Solution:
         ROWS, COLS = len(grid), len(grid[0])
 
         q = deque()
-        visited = [[0] * COLS for _ in range(ROWS)]
         res = fresh_oranges = 0
 
         for i in range(ROWS):
             for j in range(COLS):
                 if grid[i][j] == 2:
                     q.append((i, j))
-                    visited[i][j] = 1
 
                 if grid[i][j] == 1:
                     fresh_oranges += 1
@@ -26,11 +24,11 @@ class Solution:
                 i, j = q.popleft()
                 neighbors = [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]
                 for dx, dy in neighbors:
-                    if dx < 0 or dy < 0 or dx == ROWS or dy == COLS or visited[dx][dy] or grid[dx][dy] != 1:
+                    if dx < 0 or dy < 0 or dx == ROWS or dy == COLS or grid[dx][dy] != 1:
                         continue
                     
                     fresh_oranges -= 1
-                    visited[dx][dy] = 1
+                    grid[dx][dy] = 2
                     q.append((dx, dy))
 
         return res - 1 if not fresh_oranges else -1

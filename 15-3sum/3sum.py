2220650b90b9e7ef26c36ad851a75c25b6d6c850
Yash Suryavanshi <36504298+ysuryavanshi@users.noreply.github.com>
2025-04-17 13:43:26 -0700

    Time: 436 ms (90.94%) | Memory: 20.9 MB (23.22%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,31 +1,37 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ans = set()
+        z = 0
+        n, p = [], []
 
-        n, p, z = [], [], []
         for num in nums:
-            if num > 0: p.append(num)
-            elif num < 0: n.append(num)
-            else: z.append(num)
-        
+            if num == 0:
+                z += 1
+            elif num < 0:
+                n.append(num)
+            else:
+                p.append(num)
+
+        res = set()
+
         N, P = set(n), set(p)
         if z:
             for num in P:
-                if -1 * num in N:
-                    ans.add((-1 * num, 0, num))
-
-        if len(z) >= 3:
-            ans.add((0,0,0))
+                if -num in N:
+                    res.add((-num, 0, num))
+            
+        if z > 2:
+            res.add((0, 0, 0))
         
         for i in range(len(n)):
             for j in range(i + 1, len(n)):
-                target = -1 * (n[i] + n[j])
+                target = -(n[i] + n[j])
                 if target in P:
-                    ans.add(tuple(sorted([n[i], n[j], target])))
-        
+                    res.add(tuple(sorted([n[i], n[j], target])))
+
         for i in range(len(p)):
             for j in range(i + 1, len(p)):
-                target = -1 * (p[i] + p[j])
+                target = -(p[i] + p[j])
                 if target in N:
-                    ans.add(tuple(sorted([p[i], p[j], target])))
-        return list(ans)
+                    res.add(tuple(sorted([p[i], p[j], target])))
+
+        return list(res)

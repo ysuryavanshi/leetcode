781a3e3b03ab845d0bee27ba14052ebd5cf23cdc
Yash Suryavanshi <36504298+ysuryavanshi@users.noreply.github.com>
2025-04-18 12:11:36 -0700

    Time: 1 ms (47.96%) | Memory: 18 MB (32.69%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,3 +1,27 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return list(permutations(nums))
+        n = len(nums)
+        res = []
+        visited = set()
+
+        def backtrack(idx, num_list):
+            if idx == n:
+                res.append(num_list[:])
+                return
+            
+            for i in range(n):
+                if nums[i] in visited:
+                    continue
+                
+                visited.add(nums[i])
+                num_list.append(nums[i])
+
+                backtrack(idx + 1, num_list)
+
+                _ = num_list.pop()
+                visited.remove(nums[i])
+                
+            return
+
+        backtrack(0, [])
+        return res

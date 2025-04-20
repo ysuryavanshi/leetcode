c767d7f73d1e9de4462c24a303ceeef8a22fd143
Yash Suryavanshi <36504298+ysuryavanshi@users.noreply.github.com>
2025-04-20 10:56:27 -0700

    Time: 10 ms (67.85%) | Memory: 20.2 MB (52.65%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -4,19 +4,36 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        items = []
-        for l in lists:
-            while l:
-                items.append(l)
-                l = l.next
-        
-        items.sort(key=lambda x: x.val)
+    def mergeTwoLists(self, l1, l2):
         dummy = ListNode()
         head = dummy
-        for item in items:
-            head.next = item
-            head = head.next
 
-        head.next = None
+        while l1 and l2:
+            if l1.val < l2.val:
+                head.next = l1
+                l1 = l1.next
+            else:
+                head.next = l2
+                l2 = l2.next
+            head = head.next
+        
+        if l1:
+            head.next = l1
+        if l2:
+            head.next = l2
+        
         return dummy.next
+
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+        
+        if len(lists) == 1:
+            return lists[0]
+        
+        mid = len(lists) // 2
+
+        left = self.mergeKLists(lists[:mid])
+        right = self.mergeKLists(lists[mid:])
+
+        return self.mergeTwoLists(left, right)

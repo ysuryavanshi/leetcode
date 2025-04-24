274b0d63996258debfeca6d6a233e9031f659bd2
Yash Suryavanshi <36504298+ysuryavanshi@users.noreply.github.com>
2025-04-24 15:36:40 -0700

    Time: 115 ms (78.37%) | Memory: 57.5 MB (96.71%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,24 +1,33 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.data = set()
+        self.data = []
+        self.data_map = {}        
 
     def insert(self, val: int) -> bool:
-        if val in self.data:
+        if val in self.data_map:
             return False
-
-        self.data.add(val)
-        return True
+        
+        self.data_map[val] = len(self.data)
+        self.data.append(val)
+        return True        
 
     def remove(self, val: int) -> bool:
-        if val in self.data:
-            self.data.remove(val)
-            return True
+        if val not in self.data_map:
+            return False
+        
+        last_element = self.data[-1]
+        idx_val = self.data_map[val]
 
-        return False
+        self.data[idx_val] = last_element
+        self.data_map[last_element] = idx_val
+
+        _ = self.data.pop()
+        del self.data_map[val]
+        return True
 
     def getRandom(self) -> int:
-        return random.choice(list(self.data))
+        return random.choice(self.data)
         
 
 
